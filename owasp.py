87b1d03a2b465218f1f287b854d01bc0b7eb0bae
easyone97 <112688368+easyone97@users.noreply.github.com>
2024-07-04 15:17:44 +0900

    Update owasp.py

--- owasp.py
+++ owasp.py
@@ -29,17 +29,37 @@ class OWASPApp:
             {
                 "title": "탈옥공격 구문 체크리스트",
                 "description": "탈옥공격의 위험성을 체크할 수 있는 구문 616개, 유해한 질문 6개",
-                "image": "Images/checklist.jpg",
+                "image": "Images/checklist.png",
                 "file": "Downloadfile/jailbreakPrompt.csv"
             }
         ]
 
+        # 다운로드 버튼 스타일 설정
+        st.markdown("""
+            <style>
+            .download-button {
+                background-color: #87CEEB;
+                color: white;
+                border: none;
+                padding: 8px 16px;
+                text-align: center;
+                text-decoration: none;
+                display: inline-block;
+                font-size: 16px;
+                margin: 4px 2px;
+                cursor: pointer;
+                border-radius: 4px;
+            }
+            .download-button:hover {
+                background-color: #00BFFF;
+            }
+            </style>
+        """, unsafe_allow_html=True)
+
         # 참고자료 항목 표시
         for index, ref in enumerate(references):
-            with st.container(border=True):
-                col0, col1, col2, col3 = st.columns([1, 2, 6, 2])
-                with col0:
-                    st.empty()  # 빈 컬럼으로 공백 추가
+            with st.container():
+                col1, col2, col3 = st.columns([2, 6, 2], gap="medium")
                 with col1:
                     st.image(ref["image"], width=150)
                 with col2:
@@ -54,12 +74,9 @@ class OWASPApp:
                     )
                 with col3:
                     with open(ref["file"], "rb") as file:
-                        st.download_button(
-                            label="PDF 다운로드" if ref["file"].endswith(".pdf") else "CSV 다운로드", 
-                            data=file, 
-                            file_name=ref["file"].split("/")[-1], 
-                            mime="text/csv" if ref["file"].endswith(".csv") else "application/pdf",
-                            key=f"download_button_{index}"
+                        st.markdown(
+                            f'<a href="data:{"application/pdf" if ref["file"].endswith(".pdf") else "text/csv"};base64,{file.read().decode("utf-8")}" download="{ref["file"].split("/")[-1]}" class="download-button">{"PDF 다운로드" if ref["file"].endswith(".pdf") else "CSV 다운로드"}</a>',
+                            unsafe_allow_html=True
                         )
 
 if __name__ == "__main__":
