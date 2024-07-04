231bf9b05fb66f034f9c0af0aaf57c7deeb07a45
easyone97 <112688368+easyone97@users.noreply.github.com>
2024-07-04 15:23:28 +0900

    Update owasp.py

--- owasp.py
+++ owasp.py
@@ -12,6 +12,28 @@ class OWASPApp:
         st.title("참고자료")
         st.markdown("<br><br>", unsafe_allow_html=True)
 
+        # 다운로드 버튼 스타일 설정
+        st.markdown("""
+            <style>
+            .stDownloadButton > button {
+                background-color: #87CEEB;
+                color: white;
+                border: none;
+                padding: 8px 16px;
+                text-align: center;
+                text-decoration: none;
+                display: inline-block;
+                font-size: 16px;
+                margin: 4px 2px;
+                cursor: pointer;
+                border-radius: 4px;
+            }
+            .stDownloadButton > button:hover {
+                background-color: #00BFFF;
+            }
+            </style>
+        """, unsafe_allow_html=True)
+
         # 참고자료 항목들
         references = [
             {
@@ -29,15 +51,17 @@ class OWASPApp:
             {
                 "title": "탈옥공격 구문 체크리스트",
                 "description": "탈옥공격의 위험성을 체크할 수 있는 구문 616개, 유해한 질문 6개",
-                "image": "Images/checklist.png",
+                "image": "Images/checklist.jpg",
                 "file": "Downloadfile/jailbreakPrompt.csv"
             }
         ]
 
         # 참고자료 항목 표시
         for index, ref in enumerate(references):
-            with st.container():
-                col1, col2, col3 = st.columns([2, 6, 2], gap="medium")
+            with st.container(border=True):
+                col0, col1, col2, col3 = st.columns([1, 2, 6, 2])
+                with col0:
+                    st.empty()  # 빈 컬럼으로 공백 추가
                 with col1:
                     st.image(ref["image"], width=150)
                 with col2:
@@ -63,7 +87,6 @@ class OWASPApp:
 if __name__ == "__main__":
     app = OWASPApp()
     app.run()
-
 
 
 
