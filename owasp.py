291a6af118a835131695deb96633779ed412a7d1
easyone97 <112688368+easyone97@users.noreply.github.com>
2024-07-04 14:59:20 +0900

    Update owasp.py

--- owasp.py
+++ owasp.py
@@ -17,19 +17,19 @@ class OWASPApp:
             {
                 "title": "OWASP Top 10 for LLM",
                 "description": "OWASP에서 발표한 LLM 애플리케이션에 영향을 미치는 가장 치명적인 취약점 상위 10가지",
-                "image": "https://via.placeholder.com/150",
+                "image": "Images/owasp.jpeg",
                 "file": "Downloadfile/OWASP-Top-10-for-LLMs-2023-v1_1.pdf"
             },
             {
                 "title": "2024 LLM 한국형 보안 가이드라인",
                 "description": "LLM 보안 가이드라인",
-                "image": "https://via.placeholder.com/150",
-                "file": "Downloadfile/LLM_Security_Guidelines.pdf"
+                "image": "Images/가이드라인.jpeg",
+                "file": "Downloadfile/OWASP-Top-10-for-LLMs-2023-v1_1.pdf"
             },
             {
                 "title": "탈옥공격 구문 체크리스트",
                 "description": "탈옥공격의 위험성을 체크할 수 있는 구문 616개, 유해한 질문 6개",
-                "image": "https://via.placeholder.com/150",
+                "image": "https://www.kindpng.com/picc/m/376-3767580_csv-file-icon-hd-png-download.png",
                 "file": "Downloadfile/jailbreakPrompt.csv"
             }
         ]
@@ -44,9 +44,10 @@ class OWASPApp:
                 st.write(ref["description"])
             with col3:
                 with open(ref["file"], "rb") as file:
-                    st.download_button(label="PDF 다운로드", data=file, file_name=ref["file"].split("/")[-1], mime="text/csv" if ref["file"].endswith(".csv") else "application/pdf")
+                    st.download_button(label="PDF 다운로드" if ref["file"].endswith(".pdf") else "CSV 다운로드", data=file, file_name=ref["file"].split("/")[-1], mime="text/csv" if ref["file"].endswith(".csv") else "application/pdf")
 
 if __name__ == "__main__":
     app = OWASPApp()
     app.run()
+
 
