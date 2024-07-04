522d7bd227caed18ab0719e239ba375f3121d18c
easyone97 <112688368+easyone97@users.noreply.github.com>
2024-07-04 14:41:36 +0900

    Update prompt_history.py

--- prompt_history.py
+++ prompt_history.py
@@ -82,7 +82,6 @@ class PromptHistoryApp:
                 """,
                 unsafe_allow_html=True
             )
-            st.markdown("<br><br>", unsafe_allow_html=True)
             st.markdown("<h1 style='font-size: 2.5em; color: #000000;'>탈옥 프롬프트 내역</h1>", unsafe_allow_html=True)
             st.markdown("<br><br>", unsafe_allow_html=True)
 
